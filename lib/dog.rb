afc07b496a5c87aaeabcc6188416267d71264a27
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-20 18:30:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 # Your code goes here!
 class Dog
-  def initialize(name, bark)
+  def initialize(name)
     @name = name
-    @bark = bark
+
   end
 
   def name=(name)
@@ -10,6 +10,10 @@ class Dog
   end
 
   def name
-    @name 
+    @name
   end
+
+  def bark
+    puts "Woof!"
+  end 
 end
